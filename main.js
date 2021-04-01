bc92d10366c7df49a1756f629fa561ce011229f5
Kraig Hebert <kraighebert@gmail.com>
2021-03-31 22:46:39 -0400

    finished game logic

--- main.js
+++ main.js
@@ -1,62 +1,80 @@
-let possiblePlayList = ['rock', 'paper', 'scissors'];
+// randomize computer play
+const possiblePlayList = ['rock', 'paper', 'scissors'];
 const computerSelection = () => possiblePlayList[Math.floor(Math.random() * (2 - 0 + 1)) + 0];
+
+// scoreboard
 let playerScore = 0;
 let computerScore = 0;
+let roundNumber = 0;
 
-
+// play single round
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    roundNumber++
     computerSelection = computerSelection.toLowerCase();
-    console.log(`You Played: ${ playerSelection }`);
-    if (playerSelection === 'rock') {
-        switch (computerSelection) {
-            case 'rock':
-                return 'You Tie against rock.';
-            case 'paper':
-                computerScore++
-                return 'You Lose against paper.';
-            case 'scissors':
-                playerScore++;
-                return 'You Win against scissors.';
-        }
-    } else if (playerSelection === 'scissors') {
-        switch (computerSelection) {
-            case 'rock':
-                computerScore++;
-                return 'You lose against rock.';
-            case 'paper':
-                playerScore++;
-                return 'You win against paper.';
-            case 'scissors':
-                return 'You tie against scissors.';
-        }
-    } else if (playerSelection === 'paper') {
-        switch (computerSelection) {
-            case 'rock':
-                playerScore++;
-                return 'You win against rock.';
-            case 'paper':
-                return 'You tie against paper.';
-            case 'scissors':
-                computerScore++;
-                return 'You lost against scissors.';
-        }
+    switch (playerSelection) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'paper': 
+                    computerScore++;
+                    return 'comp';
+                case 'scissors': 
+                    playerScore++;
+                    return 'player';
+                default: return 'TIE';
+            }
+        case 'scissors':
+            switch (computerSelection) {
+                case 'rock': 
+                    computerScore++;
+                    return 'comp';
+                case 'paper': 
+                    playerScore++;
+                    return 'player';
+                default: return 'TIE';
+            }
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock': 
+                    playerScore++;
+                    return 'player';
+                case 'scissors': 
+                    computerScore++;
+                    return 'comp';
+                default: return 'TIE';
+            }
+
+    }
+}
+
+// alert a winner
+function finalScore () {
+    if (playerScore > computerScore) {
+        message = `You win ${ playerScore } - ${ computerScore }`;
+    } else if (playerScore < computerScore) {
+        message = `You lose ${ playerScore } - ${ computerScore }`;
     } else {
-        return 'Pick rock, paper, or scissors.';
+        message = `You tied ${ playerScore } - ${ computerScore }`;
     }
-}
-function game () {
-    for (let i = 1; i <= 5; i++) {
-        const playerSelection = prompt("Enter rock. paper, or scissors: ", "");
-        const computerSelection = () => possiblePlayList[Math.floor(Math.random() * (2 - 0 + 1)) + 0];
-        console.log(playRound(playerSelection, computerSelection()));
-    }
-
-    let message = (playerScore > computerScore) ? `You win ${ playerScore } - ${ computerScore }` :
-        (playerScore < computerScore) ? `You lose ${ playerScore } - ${ computerScore }` :
-        `You tied ${ playerScore } - ${ computerScore }`;
-    console.log(message)
+    return message
 }
 
+// set  selectors
+const setRound = document.querySelector('#round-number');
+const setWinner = document.querySelector('#winner');
+const setPlayerScore = document.querySelector('#player-score');
+const setComputerScore = document.querySelector('#comp-score');
 
-game()
+// set event listeners
+const buttons = document.querySelectorAll('.choice-btn');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let winner = playRound(e.target.id, computerSelection())
+        if (playerScore === 5 || computerScore === 5) alert(finalScore());
+        setWinner.textContent = `Round Winner: ${ winner }`;
+        setPlayerScore.textContent = `Player Score: ${ playerScore }`;
+        setComputerScore.textContent = `Computer Score: ${ computerScore }`;
+        setRound.textContent = `Round ${ roundNumber }`;
+    });
+});
+
+
